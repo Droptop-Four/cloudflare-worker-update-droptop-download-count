6efa431e2b67927900923a36a4d338fc825c507c
Bunz <66bunz@gmail.com>
2024-03-06 22:55:11 +0100

    Added supporter download counter

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+
 # Cloudflare Worker Update Droptop Download Count
-This Cloudflare Worker updates every 20min the number of times Droptop Four was downloaded.
+
+This Cloudflare Worker updates every hour the number of times Droptop Four was downloaded.
 
 It gets the number of downloads from the number of downloads of the assets of every release of Droptop Four from the [Droptop-Four/Droptop-Four](https://github.com/Droptop-Four/Droptop-Four) repo.
 
+It also gets the number of downloads of the Supporter version using the Gumroad API.
+
 Then it uploads the number of downloads in a mongodb atlas database using the REALM API.
+
+
+# Development
+
+To start the dev server run `npm run dev`, to simulate a cron trigger, use `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"`.
--- src/index.js
+++ src/index.js
@@ -1,34 +1,31 @@
-import * as Realm from "realm-web";
+import * as Realm from 'realm-web';
 
 const {
-    BSON: { ObjectId },
+	BSON: { ObjectId },
 } = Realm;
 
-
-async function login(appId, apiKey) {
-    const app = new Realm.App({ id: appId });
-    const credentials = Realm.Credentials.apiKey(apiKey);
-    const user = await app.logIn(credentials);
-    console.assert(user.id === app.currentUser.id);
-    return user;
+async function realmLogin(appId, apiKey) {
+	const app = new Realm.App({ id: appId });
+	const credentials = Realm.Credentials.apiKey(apiKey);
+	const user = await app.logIn(credentials);
+	console.assert(user.id === app.currentUser.id);
+	return user;
 }
 
-
-async function getReleasesDownloads(privateKey) {
-
+async function getReleasesDownloads(apiKey) {
 	async function fetchData(url) {
 		let allReleases = [];
 		let page = 1;
 		let response;
-	
+
 		do {
 			response = await fetch(`${url}?page=${page}`, {
-				method: "GET",
+				method: 'GET',
 				headers: {
-					Accept: "application/vnd.github+json",
-					Authorization: `Bearer ${privateKey}`,
-					"X-GitHub-Api-Version": "2022-11-28",
-					"User-Agent": "update-droptop-download-count",
+					Accept: 'application/vnd.github+json',
+					Authorization: `Bearer ${apiKey}`,
+					'X-GitHub-Api-Version': '2022-11-28',
+					'User-Agent': 'update-droptop-download-count',
 				},
 			});
 			if (!response.ok) {
@@ -42,38 +39,63 @@ async function getReleasesDownloads(privateKey) {
 			}
 			page++;
 		} while (true);
-	
+
 		return allReleases;
 	}
-	
-    const releasesUrl = "https://api.github.com/repos/Droptop-Four/Droptop-Four/releases";
-    const releases = await fetchData(releasesUrl);
-	
-    let basic_downloads = 0;
-    let update_downloads = 0;
-	
-    for (const release of releases) {
-        if (release.assets.length >= 2) {
+
+	const releasesUrl = 'https://api.github.com/repos/Droptop-Four/Droptop-Four/releases';
+	const releases = await fetchData(releasesUrl);
+
+	let basic_downloads = 0;
+	let update_downloads = 0;
+
+	for (const release of releases) {
+		if (release.assets.length >= 2) {
 			basic_downloads += release.assets[0].download_count;
 			update_downloads += release.assets[1].download_count;
-        }
-    }
+		}
+	}
 
-    return { basic_downloads, update_downloads };
+	return { basic_downloads, update_downloads };
 }
 
+async function getSupporterDownloads(apiKey) {
+	async function fetchData(url) {
+		let response;
+
+		response = await fetch(`${url}`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${apiKey}`,
+			},
+		});
+		if (!response.ok) {
+			throw new Error(`Failed to fetch data: ${response.statusText}`);
+		}
+		const data = await response.json();
+
+		return data.products[0].sales_count;
+	}
+
+	const url = 'https://api.gumroad.com/v2/products';
+	const data = await fetchData(url);
+
+	return data;
+}
 
 export default {
-    async scheduled(event, env, ctx) {
-        const user = await login(env.REALM_APPID, env.REALM_APIKEY);
-        const collection = user.mongoClient("mongodb-atlas").db(env.DB).collection(env.COLLECTION);
+	async scheduled(event, env, ctx) {
+		const user = await realmLogin(env.REALM_APPID, env.REALM_APIKEY);
+		const collection = user.mongoClient('mongodb-atlas').db(env.DB).collection(env.COLLECTION);
 
-        try {
-            const { basic_downloads, update_downloads } = await getReleasesDownloads(env.GITHUB_APIKEY);
-            await collection.updateOne({ title: "downloads" }, { $set: { basic_downloads, update_downloads } });
-            console.log("Downloads updated successfully");
-        } catch (error) {
-            console.error("Error updating downloads:", error.message);
-        }
-    },
+		try {
+			const { basic_downloads, update_downloads } = await getReleasesDownloads(env.GITHUB_APIKEY);
+			const supporter_downloads = await getSupporterDownloads(env.GUMROAD_APIKEY);
+			await collection.updateOne({ title: 'downloads' }, { $set: { basic_downloads, update_downloads, supporter_downloads } });
+			console.log('Downloads updated successfully');
+		} catch (error) {
+			console.error('Error updating downloads:', error.message);
+		}
+	},
 };
